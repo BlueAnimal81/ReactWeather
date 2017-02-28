fcdef90b89ec40a4f01101662b928e2755a73fef
BlueAnimal81 <blueanimal@me.com>
2017-02-28 08:25:37 +0000

    Add nav search support

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,8 +4,14 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function(e) {
     e.preventDefault();
-    alert('Not implemented');
+    var location = this.refs.search.value;
+    var encodedLocation = encodeURIComponent(location);
+    if (location.length > 0) {
+      this.refs.search.value = '';
+      window.location.hash = `#/?location=${encodedLocation}`;
+    }
   },
+
   render: function() {
     return (
       <div className="top-bar">
@@ -27,7 +33,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weather by city"/>
+                <input type="search" placeholder="Search weather by city" ref="search"/>
               </li>
               <li>
                 <button type="submit" className="button" value="Get Weather" >Search</button>
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -11,10 +11,30 @@ var Weather = React.createClass({
       };
   },
 
+  componentDidMount: function() {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash= '#/';
+    }
+  },
+
+  componentWillReceiveProps: function(newProps) {
+		var location = newProps.location.query.location;
+
+		if (location && location.length > 0) {
+			this.handleSearch(location);
+			window.location.hash = '#/';
+		}
+	},
+
   handleSearch: function(location) {
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
 
     openWeatherMap.getTemp(location).then(
